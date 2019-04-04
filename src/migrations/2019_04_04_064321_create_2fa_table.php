09e3fd1339f345b11658f0dbca1f276e2c675a20
Wiebe Nieuwenhuis <wiebenieuwenhuis@gmail.com>
2019-04-04 11:56:30 +0200

    fix

--- src/migrations/2019_04_04_064321_create_2fa_table.php
+++ src/migrations/2019_04_04_064321_create_2fa_table.php
@@ -17,7 +17,7 @@ class Create2faTable extends Migration
             $table->bigIncrements('id');
             $table->string('model_id');
             $table->string('model_type');
-            $table->string('secret')->unique();
+            $table->text('secret')->unique();
             $table->boolean('enabled')->default(false);
             $table->unique(['model_id', 'model_type']);
         });
