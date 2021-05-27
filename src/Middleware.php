a271b78a3e1535194d9236d65d2cf4c24481c5dd
Wiebe Nieuwenhuis <wiebenieuwenhuis@gmail.com>
2021-05-27 16:34:24 +0200

    check if 2fa is required

--- src/Middleware.php
+++ src/Middleware.php
@@ -8,10 +8,20 @@ class Middleware
 {
 	public function handle($request, Closure $next)
 	{
-		// Check if the user is authenticated by 2fa
-		if(Laravel2fa::authenticated() || in_array($request->route()->uri, config('2fa.exclude_urls', []))){
-			return $next($request);
+		// Check url is excluded
+		if(in_array($request->route()->uri, config('2fa.exclude_urls', []))){
+		    return $next($request);
 		}
+
+		// Check if 2fa required and not enabled
+		if(config('2fa.required') && !Laravel2fa::enabled()){
+		    return redirect(route('2fa::setup'));
+		}
+
+		// Check if the user is authenticated by 2fa
+        	if(Laravel2fa::authenticated()){
+            	    return $next($request);
+        	}
 
 		// Not authenticated, redirect to 2fa auth route
 		return redirect(config('2fa.auth_route'));
